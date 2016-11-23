ec8ac469b5f2b029adcc04f25e3fc9411781024d
Honglin Yu <yuhonglin1986@gmail.com>
2016-11-23 20:19:45 +1100

    add key bindings

--- matlab-mode.el
+++ matlab-mode.el
@@ -20,6 +20,12 @@
 		 (flycheck-mode)
 		 ;; use flycheck only when saving the file or enabling the mode
 		 (setq-local flycheck-check-syntax-automatically '(save mode-enabled))
+		 ;; bind the key of checking document
+		 (local-set-key (kbd "C-c h") 
+				'matlab-view-current-word-doc-in-another-buffer)
+		 ;; bind the key of jump to source code
+		 (local-set-key (kbd "C-c s") 
+				'matlab-jump-to-definition-of-word-at-cursor)
 		 ;; set company-backends
 		 (setq-local company-backends '(company-files (company-matlab company-dabbrev)))))
 
